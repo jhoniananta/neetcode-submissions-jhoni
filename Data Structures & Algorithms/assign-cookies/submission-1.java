c9c9f36fc8a660b1fe667398aa9fd805986b6059
Jhoni Ananta Sitepu <93531492+jhoniananta@users.noreply.github.com>
2026-05-01 21:51:13 +0700

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        int res = 0;
+        for (int i : g) {
+            int minIdx = -1;
+            for (int j = 0; j < s.length; j++) {
+                if (s[j] < i) continue;
+
+                if (minIdx == -1 || s[minIdx] > s[j]) {
+                    minIdx = j;
+                }
+            }
+
+            if (minIdx != -1) {
+                s[minIdx] = -1;
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
